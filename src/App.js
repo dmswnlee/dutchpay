257833bbaed83e16087a02279f514a0b03c35986
LeeEunJoo <dlrmawnl03@gmail.com>
2024-08-29 23:05:04 +0900

    그룹 생성 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CreateGroup from './components/CreateGroup';
-import AddMembers from './components/AddMembers';
-import ExpenseMain from './components/ExpenseMain';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateGroup from "./components/CreateGroup";
+import AddMembers from "./components/AddMembers";
+import ExpenseMain from "./components/ExpenseMain";
+import { RecoilRoot } from "recoil";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<CreateGroup />} />
-        <Route path='/members' element={<AddMembers />} />
-        <Route path='/expense' element={<ExpenseMain />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<RecoilRoot>
+				<Routes>
+					<Route path="/" element={<CreateGroup />} />
+					<Route path="/members" element={<AddMembers />} />
+					<Route path="/expense" element={<ExpenseMain />} />
+				</Routes>
+			</RecoilRoot>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import CenteredOverlayForm from './CenteredOverlayForm'
+import React, { useState } from "react";
+import CenteredOverlayForm from "./CenteredOverlayForm";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { groupNameState } from "../state/groupName";
 
 const CreateGroup = () => {
-  return (
-    <div>
-      Create Group component
-      <CenteredOverlayForm />  
-    </div>
-  )
-}
+	const [validated, setValidated] = useState(false);
+	const [validGroupName, setValidGroupName] = useState(false);
+	const [groupName, setGroupName] = useRecoilState(groupNameState);
 
-export default CreateGroup
+	const handleSubmit = event => {
+		event.preventDefault();
+
+		const form = event.currentTarget;
+		if (form.checkValidity()) {
+			setValidGroupName(true);
+		} else {
+			event.stopPropagation();
+			setValidGroupName(false);
+		}
+		setValidated(true);
+	};
+
+	return (
+		<div>
+			<h1>Dutch Pay</h1>
+			{/* <CenteredOverlayForm />   */}
+			<Container>
+				<Form noValidate validated={validated} onSubmit={handleSubmit}>
+					<Row>
+						<h2>먼저, 더치 페이 할 그룹의 이름을 정해볼까요?</h2>
+					</Row>
+					<Form.Group controlId="validationGroupName">
+						<Form.Control
+							type="text"
+							required
+							placeholder="2022 제주도 여행"
+							onChange={e => setGroupName(e.target.value)}
+						/>
+						<Form.Control.Feedback type="invalid" data-valid={validGroupName}>
+							그룹 이름을 입력해 주세요.
+						</Form.Control.Feedback>
+					</Form.Group>
+					<Row>
+						<Button type="submit">저장</Button>
+					</Row>
+				</Form>
+			</Container>
+		</div>
+	);
+};
+
+export default CreateGroup;
