605f8d7105b3ed585b9c0f8b7cfd0e9819306514
LeeEunJoo <dlrmawnl03@gmail.com>
2024-08-30 18:26:06 +0900

    스타일링 리팩토링

--- src/components/CenteredOverlayForm.jsx
+++ src/components/CenteredOverlayForm.jsx
@@ -1,29 +1,79 @@
-import { Container } from "react-bootstrap";
+import { Button, Container, Form, Row } from "react-bootstrap";
 import styled from "styled-components";
 import OverlayWrapper from "./shared/OverlayWrapper";
 
-const CenteredOverlayForm = ({ children }) => {
+const CenteredOverlayForm = ({ title, children, validated, handleSubmit }) => {
 	return (
 		<StyledCentralizedContainer>
-			<StyledHeader>Dutch Pay</StyledHeader>
-			<OverlayWrapper>{children}</OverlayWrapper>
+			<StyledLogo>Dutch Pay</StyledLogo>
+			<OverlayWrapper>
+				<Container>
+				<Form noValidate validated={validated} onSubmit={handleSubmit}>
+					<StyledRow>
+						<Row className="aligin-items-start">
+							<StyledTitle>{title}</StyledTitle>
+						</Row>
+						<Row className="aligin-items-center">
+							{children}
+						</Row>
+						<Row className="aligin-items-end">
+							<StyledSubmitButton>저장</StyledSubmitButton>
+						</Row>
+					</StyledRow>
+				</Form>
+			</Container>
+			</OverlayWrapper>
 		</StyledCentralizedContainer>
 	);
 };
 
 export default CenteredOverlayForm;
 
-const StyledHeader = styled.h1`
+const StyledLogo = styled.h1`
   font-weight: 200;
+	letter-spacing: 10px;
+	color: slateblue;
+	text-align: center;
+	margin-bottom: 0.8em;
 `
 
 const StyledCentralizedContainer = styled(Container)`
 	width: 50vw;
 	min-height: 100vh;
+
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	padding: 0;
 	gap: 10px;
+`;
+
+const StyledTitle = styled.h2`
+	font-weight: 700;
+	line-height: 35px;
+
+	text-align: right;
+	overflow-wrap: break-word;
+	word-break: keep-all;
+`;
+
+const StyledSubmitButton = styled(Button).attrs({ type: "submit" })`
+	width: 60%;
+	height: 50px;
+	margin: 0 auto;
+	background-color: #6610f2;
+	border-radius: 8px;
+	border: none;
+
+	&:hover {
+		background-color: #6610f2;
+		filter: brightness(80%);
+	}
+`;
+
+const StyledRow = styled(Row)`
+	align-items: center;
+	justify-content: center;
+	height: 60vh;
 `;
--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import CenteredOverlayForm from "./CenteredOverlayForm";
-import { Button, Container, Form, Row } from "react-bootstrap";
+import CenteredOverlayForm, { StyledH2, StyledRow, StyledSubmitButton } from "./CenteredOverlayForm";
+import { Form } from "react-bootstrap";
 import { useSetRecoilState } from "recoil";
 import { groupNameState } from "../state/groupName";
-import styled from "styled-components";
 
 const CreateGroup = () => {
 	const [validated, setValidated] = useState(false);
@@ -24,59 +23,23 @@ const CreateGroup = () => {
 	};
 
 	return (
-		<CenteredOverlayForm>
-			<Container>
-				<Form noValidate validated={validated} onSubmit={handleSubmit}>
-					<StyledRow>
-						<Row className="aligin-items-start">
-							<StyledH2>먼저, 더치 페이 할 그룹의 이름을 정해볼까요?</StyledH2>
-						</Row>
-						<Row className="aligin-items-center">
-							<Form.Group controlId="validationGroupName">
-								<Form.Control
-									type="text"
-									required
-									placeholder="2022 제주도 여행"
-									onChange={e => setGroupName(e.target.value)}
-								/>
-								<Form.Control.Feedback type="invalid" data-valid={validGroupName}>
-									그룹 이름을 입력해 주세요.
-								</Form.Control.Feedback>
-							</Form.Group>
-						</Row>
-						<Row className="aligin-items-end">
-							<StyledSubmitButton>저장</StyledSubmitButton>
-						</Row>
-					</StyledRow>
-				</Form>
-			</Container>
+		<CenteredOverlayForm
+			title={"먼저, 더치 페이 할 그룹의 이름을 정해볼까요?"}
+			validated={validated}
+			handleSubmit={handleSubmit}>
+			<Form.Group controlId="validationGroupName">
+				<Form.Control
+					type="text"
+					required
+					placeholder="2022 제주도 여행"
+					onChange={e => setGroupName(e.target.value)}
+				/>
+				<Form.Control.Feedback type="invalid" data-valid={validGroupName}>
+					그룹 이름을 입력해 주세요.
+				</Form.Control.Feedback>
+			</Form.Group>
 		</CenteredOverlayForm>
 	);
 };
 
 export default CreateGroup;
-
-const StyledH2 = styled.h2`
-	font-weight: 700;
-	line-height: 35px;
-	text-align: right;
-	overflow-wrap: break-word;
-	word-break: keep-all;
-`;
-
-const StyledSubmitButton = styled(Button).attrs({ type: "submit" })`
-	background-color: #6610f2;
-	border-radius: 8px;
-	border: none;
-
-	&:hover {
-		background-color: #6610f2;
-		filter: brightness(80%);
-	}
-`;
-
-const StyledRow = styled(Row)`
-	align-items: center;
-	justify-content: center;
-	height: 60vh;
-`;
